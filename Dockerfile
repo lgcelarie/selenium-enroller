6484ce791cc9f640e980ccc77ebf70d6bc422bdc
Guillermo Celarie <lg.celarie@gmail.com>
2023-05-07 21:23:46 -0600

    optimizations in Dockerfile. - Adding basic functionality in app.py - Empty requirements.txt

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,18 @@
 FROM public.ecr.aws/lambda/python:3.10
-
-# installing the browser and the webdriver
-RUN yum install -y wget unzip
-RUN cd /tmp/ &&  \
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm && \
-yum localinstall -y google-chrome-stable_current_x86_64.rpm
+WORKDIR /tmp
+# installing the browser
+RUN yum update && install -y wget unzip
+RUN wget https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm && \
+yum localinstall -y google-chrome-stable_current_x86_64.rpm && \
+rm -rf google-chrome-stable_current_x86_64.rpm
+# installing the chromedriver
 RUN chromever=$(google-chrome-stable --version | awk -F " " '{print $3}') && \
 wget https://chromedriver.storage.googleapis.com/LATEST_RELEASE_${chromever%.*} -O chromedriverver && \
 wget https://chromedriver.storage.googleapis.com/$(cat chromedriverver)/chromedriver_linux64.zip && \
 unzip chromedriver_linux64.zip && \
-mv chromedriver /usr/bin/chromedriver
+mv chromedriver /usr/bin/chromedriver && \
+chmod +x /usr/bin/chromedriver && \
+rm -rf chromedriver_linux64.zip
 # installing requirements
 # COPY app/requirements.txt  .
 # RUN  pip3 install -r requirements.txt --target "${LAMBDA_TASK_ROOT}"
--- app/app.py
+++ app/app.py
@@ -1,3 +1,67 @@
+import json
+import boto3
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
 
-driver = webdriver.Chrome("/usr/bin/chromedriver")
+s3 = boto3.client('s3')
+
+def lambda_handler(event, context):
+    # Descarga el archivo clientes.json del bucket S3
+    bucket_name = 'selenium-enroller'
+    file_name = 'clientes.json'
+    s3.download_file(bucket_name, file_name, '/tmp/' + file_name)
+    
+    # Abre el archivo clientes.json y lee los datos
+    with open('/tmp/' + file_name, 'r') as f:
+        clientes = json.load(f)
+    
+    # Configura el driver de Chrome
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    driver = webdriver.Chrome(options=options)
+    
+    # Navega a la página web
+    driver.get("https://servicios.comedica.com.sv/ReportaCompraTC-war/")
+    
+    # Recorre la lista de clientes y envía el formulario para cada uno
+    for cliente in clientes:
+        nombre = cliente['nombre']
+        email = cliente['email']
+        telefono = cliente['telefono']
+        monto = cliente['monto']
+        tipo_tarjeta = cliente['tipo_tarjeta']
+        
+        # Encuentra los campos del formulario y los completa
+        nombre_input = driver.find_element(By.NAME, "nombre")
+        nombre_input.send_keys(nombre)
+        email_input = driver.find_element(By.NAME, "email")
+        email_input.send_keys(email)
+        telefono_input = driver.find_element(By.NAME, "telefono")
+        telefono_input.send_keys(telefono)
+        monto_input = driver.find_element(By.NAME, "monto")
+        monto_input.send_keys(monto)
+        select = Select(driver.find_element(By.NAME, "tipo_tarjeta"))
+        select.select_by_value(tipo_tarjeta)
+        
+        # Envía el formulario
+        submit_button = driver.find_element(By.NAME, "submit")
+        submit_button.click()
+        
+        # Limpia los campos del formulario para el siguiente cliente
+        nombre_input.clear()
+        email_input.clear()
+        telefono_input.clear()
+        monto_input.clear()
+    
+    # Cierra el navegador
+    driver.quit()
+    
+    # Retorna la respuesta
+    response = {
+        'statusCode': 200,
+        'body': json.dumps({'message': 'Formularios enviados correctamente'})
+    }
+    return response
