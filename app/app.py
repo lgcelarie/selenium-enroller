799a942c338a6c76783d441f6f4a7d31497a0e9d
Guillermo Celarie <lg.celarie@gmail.com>
2023-05-11 13:54:30 -0600

    Proper usage of selenium select to obtain options

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-import json, datetime, locale, boto3, os
+import json, datetime, locale, boto3, os, logging
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
@@ -14,21 +14,17 @@ def obtain_promos(driver):
     current_mont_and_year = datetime.datetime.now().strftime('%B %Y').upper()
 
     driver.get("https://servicios.comedica.com.sv/ReportaCompraTC-war/")
-    select_options = Select(driver.find_element(By.NAME, "promocion")).getOptions()
+    select_options = Select(driver.find_element(By.NAME, "promocion")).options
 
     for word in keywords:
         for option in select_options:
-            text = option.getText()
-            if text != None and text.contains(current_mont_and_year) and text.contains(word):
-                return_dict[word] = option.getValue()
+            text = option.text
+            if text != None and current_mont_and_year in text and word in text:
+                return_dict[word] = option.text
 
     return return_dict
 
 def lambda_handler(event, context):
-    # Seteando locale a espanol
-    locale.setlocale(locale.LC_TIME, 'es_ES.UTF-8')
-    # Obteniendo mes y anio actual en mayuscula para busqueda por texto
-    current_mont_and_year = datetime.datetime.now().strftime('%B %Y').upper()
     # Descarga el archivo clientes.json del bucket S3
     bucket_name = 'selenium-enroller'
     file_name = 'clientes.json'
@@ -49,7 +45,7 @@ def lambda_handler(event, context):
     driver = webdriver.Chrome(options=options)
 
     available_promos = obtain_promos(driver)
-
+    logging.info(f'promos:{available_promos}')
     # Navega a la página web
     # driver.get("https://servicios.comedica.com.sv/ReportaCompraTC-war/")
 
@@ -68,7 +64,7 @@ def lambda_handler(event, context):
                 digitos_input = driver.find_element(By.NAME, "digitos")
                 digitos_input.send_keys(digitos)
                 select_promocion = Select(driver.find_element(By.NAME, "promocion"))
-                select_promocion.select_by_value(available_promos[promo])
+                select_promocion.select_by_visible_text(available_promos[promo])
 
                 # Envía el formulario
                 submit_button = driver.find_element(By.CLASS_NAME, "contact100-form-btn")
@@ -79,8 +75,10 @@ def lambda_handler(event, context):
 
                 response = driver.page_source
                 if 'Felicidades, ya te encuentras suscrito a la promoción, se tomarán en cuenta todas las compras que apliquen.' in response:
+                    logging.info(f'El formulario para {socio} con promocion {available_promos[promo]} se envió correctamente')
                     print(f'El formulario para {socio} con promocion {available_promos[promo]} se envió correctamente')
                 else:
+                    logging.warning(f'Hubo un error al enviar el formulario para {socio} con promocion {available_promos[promo]}')
                     print(f'Hubo un error al enviar el formulario para {socio} con promocion {available_promos[promo]}')
 
                 # Limpia los campos del formulario para el siguiente cliente
